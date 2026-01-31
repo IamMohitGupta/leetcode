39ed2bbc34a6957ce791b3fb3cd05cbb4a7dfa2d
Mohit Gupta <72707911+IamMohitGupta@users.noreply.github.com>
2026-01-31 10:43:37 -0700

    Update 11. Container With Most Water.py

--- 11. Container With Most Water.py
+++ 11. Container With Most Water.py
@@ -15,3 +15,18 @@ class Solution(object):
             else:
                 right-=1
         return maxarea
+
+"""
+
+Approach:
+
+2 pointer: 
+Initial left pointer at position 0
+Initial right pointer at the end of height list
+
+Maximum area can be min height of left and right pointed x the width between them.
+Assuming maxarea to be 0 initially, change maxarea to be maximum of above calculated area and maxarea
+
+If height of left pointer is smaller than right pointer then move left pointer up by 1, else decrease right pointer by 1
+
+"""
